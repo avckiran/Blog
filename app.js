607b790c834e44ec326484da908a909401f4b93e
Chandra Kiran Achanta <avckiran>
2019-05-16 20:06:08 -0500

    all routes completed, UI changes remaining

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
 const   express = require('express'),
         mongoose = require('mongoose'),
         bodyParser = require('body-parser'),
-        cfg = require('./config/config.js'),
-        mysql = require('mysql'),
+        methodOverride = require('method-override'),
+        expressSanitizer = require('express-sanitizer'),
         app = express();
 
+// App config
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 
-// Database initialization -- MySQL
-const db = mysql.createConnection(cfg.dbCreds);
+// Database initialization -- Mongoose
 
-db.connect(err=>{
-    if(err){console.log("ERROR!"+err);}else{console.log("Connected MySQL Successfully!")}
-})
-// Database has been setup
-
-blogs = [];
-db.query("SELECT * FROM X_BLOG", (err,result)=>{
-if(!err){blogs.push(result);}
+mongoose.connect("mongodb://localhost/blogApp",{
+        useNewUrlParser:true, 
+        useCreateIndex:true,
+        useFindAndModify: false
+    }, function(err){
+    if(err){
+        console.log(err);
+    }else { console.log ("Connected to Mongo");}
 });
+
+// Create schema and model
+
+const blogSchema = new mongoose.Schema(
+    {
+        title: String,
+        image: String,
+        body: String,
+        created: {type: Date, default:Date.now}
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// Testing the db to create a blog
+
+// Blog.create({
+//     title:"Test post1",
+//     image:"https://cdn.pixabay.com/photo/2018/10/30/16/06/water-lily-3784022__340.jpg",
+//     body:"This is a sample blog to test"
+// });
 
 
 //Routes
@@ -32,40 +54,76 @@ app.get('/', (req,res)=>{
 });
 
 app.get("/blogs", (req,res)=>{
-    res.render('blogs', {blogs:blogs});
-    // console.log(blogs);
+   Blog.find({}, function(err, result){
+       if(!err){
+           res.render('index', {blogs: result});
+       }else{ 
+           console.log ("Error! : ", err);
+       }
+   }); 
 });
 
 app.get("/blogs/new", (req,res)=>{
     res.render('new');
 });
 
-app.get("/blogs/:id", (req,res)=>{
-    // sql="SELECT * FROM X_BLOG WHERE ID='?'";
-    // value=req.params.id;
-    // db.query(sql,value,(err,result)=>{
-    //     if(err){
-    //         res.redirect("/blogs");
-    //         console.log(err);
-    //     }else{
-    //         res.render("show",{result: result});
-    //         console.log(result);
-    //     }
-    // });
-    res.render("show", {blogs:blogs});
-});
+// Create Route
 
 app.post('/blogs',(req,res)=>{
-    let sql="INSERT INTO X_BLOG(title, image, text) VALUES(?,?,?)";
-    let values=[req.body.blog['title'],req.body.blog['image'],req.body.blog['desc']];
-    db.query(sql,values,(err,result)=>{
+    // create blog
+        req.body.blog.body = req.santize(req.body.blog.body);
+        Blog.create(req.body.blog, function(err, newBlog){
+            if(!err){
+                res.redirect('/blogs')
+            }else{
+                res.render('new');
+                console.log("Error!", err);
+            }
+        });
+});
+
+app.get("/blogs/:id", (req,res)=>{
+    Blog.findById(req.params.id, function(err, result){
+        if(!err){
+            res.render("show", {blog: result});
+        }else{res.redirect("/blogs");}
+    });
+    // res.render("show");
+});
+
+// Edit route
+
+app.get("/blogs/:id/edit", function(req,res){
+    Blog.findById(req.params.id, function(err, result){
+        if(err){ res.redirect("/blogs");}else{
+         res.render("edit", {blog: result});   
+        }
+    })
+    // res.render("edit");
+})
+
+
+//Update Route
+app.put("/blogs/:id", function(req,res){
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog){
         if(err){
-            console.log("ERROR!", err);
+            console.log(err);
+            res.redirect("/blogs");
         }else{
-            console.log("Row inserted");
+            res.redirect("/blogs/"+req.params.id);
+        }
+   }); 
+});
+
+//Delete Route
+app.delete("/blogs/:id", function (req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+        console.log(err);
+        res.redirect("/blogs");
         }
     });
-    res.redirect('/blogs');
+    res.redirect("/blogs");
 });
 
 
